fd573a45bff3473ba4efce0d17c8ea52798db5de
Sohum Berry <sohumberry@gmail.com>
2024-09-11 21:02:31 -0700

    Second log

--- src/City.java
+++ src/City.java
@@ -5,25 +5,40 @@ public class City {
     private ArrayList<Integer> possibleConnections;
     private boolean hasHospital;
     private int cityNum;
+    private boolean isBFSVisited;
 
     public City(int cityNum, ArrayList<Integer> possibleConnections) {
         this.hasHighway = false;
         this.possibleConnections = possibleConnections;
         this.hasHospital = false;
         this.cityNum = cityNum;
+        this.isBFSVisited = false;
     }
 
 
     public String toString() {
-        String out = "City #" + cityNum + " does " + (hasHighway ? "" : "not ") + "have a highway.\n";
-        out += "City #" + cityNum + " does " + (hasHospital ? "" : "not ") + "have a hospital.\n";
-        out += "Possible connections:\n" + possibleConnections;
-
-        return out;
+        return "" + cityNum;
+//        String out = "City #" + cityNum + " does " + (hasHighway ? "" : "not ") + "have a highway.\n";
+//        out += "City #" + cityNum + " does " + (hasHospital ? "" : "not ") + "have a hospital.\n";
+//        out += "Possible connections:\n" + possibleConnections;
+//
+//        return out;
     }
 
     public int getNumHighways() {
         return possibleConnections.size();
+    }
+
+    public boolean isBFSVisited() {
+        return isBFSVisited;
+    }
+
+    public void setBFSVisited(boolean BFSVisited) {
+        isBFSVisited = BFSVisited;
+    }
+
+    public ArrayList<Integer> getPossibleConnections() {
+        return possibleConnections;
     }
 
     public void giveHospital() {
--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,5 +1,8 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Highways & Hospitals
@@ -19,11 +22,12 @@ public class HighwaysAndHospitals {
      */
     private static ArrayList<Integer>[] makeMap(int[][] cities, int n) {
         ArrayList<Integer>[] map = new ArrayList[n+1];
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n+1; i++) {
             map[i] = new ArrayList<Integer>();
         }
-        for (int i = 0; i < cities.length; i++) {
-            map[cities[i][0]].add(cities[i][1]);
+        for (int[] city : cities) {
+            map[city[0]].add(city[1]);
+            map[city[1]].add(city[0]);
         }
 
         return map;
@@ -38,26 +42,69 @@ public class HighwaysAndHospitals {
             return (long) hospitalCost * n;
         }
 
-        int numHospitals = 0;
+        int numHospitals = 0,
+            numHighways = 0;
 
         ArrayList<Integer>[] map = makeMap(cities, n);
-        City[] cityClasses = new City[n];
-        for (int i = 0; i < n; i++) {
+//        System.out.println(Arrays.toString(map));
+        // 1-indexed
+        City[] cityClasses = new City[n+1];
+        for (int i = 1; i < n+1; i++) {
             cityClasses[i] = new City(i, map[i]);
         }
-        for (City c : cityClasses) {
-            if (c.getNumHighways() == 0) {
+//        System.out.println(Arrays.toString(cityClasses));
+        for (int i = 1; i < cityClasses.length; i++) {
+            if (cityClasses[i].getNumHighways() == 0) {
                 numHospitals ++;
-                c.giveHospital();
+                cityClasses[i].giveHospital();
             }
-            System.out.println(c);
         }
-
-        int mostConnections = Integer.MIN_VALUE;
-        for (City c : cityClasses) {
-
+        ArrayList<ArrayList<City>> clusters = new ArrayList<>();
+        // maybe bfs to find each city cluster...
+        int length = -1;
+        while (length != 0) {
+            ArrayList<City> cluster = BFS(cityClasses);
+            length = cluster.size();
+            if (length != 0) {
+                clusters.add(cluster);
+            }
         }
+//        int mostConnections = Integer.MIN_VALUE;
+//        for (City c : cityClasses) {
+//
+//        }
 
-        return (long) numHospitals*hospitalCost;
+        return (long) numHospitals*hospitalCost + numHighways*highwayCost;
+    }
+
+    private static ArrayList<City> BFS(City[] cities) {
+        ArrayList<City> cluster = new ArrayList<>();
+        Queue<City> possibilities = new LinkedList<>();
+        int i = -1;
+        for (int j = 1; j < cities.length; j++) {
+            if (!cities[j].isBFSVisited()) {
+                i = j;
+                break;
+            }
+        }
+        // Return empty arraylist if all cities have been checked
+        if (i == -1) {
+            return new ArrayList<City>();
+        }
+        possibilities.add(cities[i]);
+        cities[i].setBFSVisited(true);
+        cluster.add(cities[i]);
+        while (!possibilities.isEmpty()) {
+            ArrayList<Integer> connections = possibilities.remove().getPossibleConnections();
+            for (int city : connections) {
+                if (!cities[city].isBFSVisited()) {
+                    possibilities.add(cities[city]);
+                    cities[city].setBFSVisited(true);
+                    cluster.add(cities[city]);
+                }
+            }
+        }
+        System.out.println(cluster);
+        return cluster;
     }
 }
